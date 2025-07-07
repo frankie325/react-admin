4760e2fc7210d166667e486ec33f1ba317a19d31
frank <fugangkuang4@gmail.com>
2025-07-08 00:59:11 +0800

    🐞 fix: 修改http异常消息获取

--- react-admin-back/src/common/filters/http-exception.filter.ts
+++ react-admin-back/src/common/filters/http-exception.filter.ts
@@ -6,7 +6,6 @@ import {
 } from '@nestjs/common';
 import { Response } from 'express';
 import { ResponseResult } from '@/utils/response';
-
 // 捕获HTTP异常的过滤器
 // 该过滤器会在发生HTTP异常时被调用，并返回统一的错误响应
 @Catch(HttpException)
@@ -15,11 +14,24 @@ export class HttpExceptionFilter implements ExceptionFilter {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
     const status = exception.getStatus();
+    // console.log('----response-----', response);
+    // console.log('----exception----', exception);
+    // console.log('---------', exception.getResponse());
+    const res = exception.getResponse();
 
-    console.log(exception);
+    let message: string = '';
+    if (typeof res === 'string') {
+      message = res;
+    } else if ('message' in res) {
+      message = Array.isArray(res.message)
+        ? res.message.join('; ')
+        : (res.message as string);
+    } else {
+      message = '';
+    }
 
     response
       .status(status)
-      .json(ResponseResult.error(null, -1, status, exception.message));
+      .json(ResponseResult.error(null, -1, status, message));
   }
 }
